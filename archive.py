d0240636b9147a26c48e9aa43af5da564ab6cd3c
krunch3r76 <biz@u26a4.com>
2022-05-18 17:48:54 -0700

    documentation and some refactor

--- archive.py
+++ archive.py
@@ -1,16 +1,13 @@
-"""provides an archive function to tar an input list
-
+"""provides an archive function to tar an input list of a directory or several files
 
 requirements:
-    gompress shall emulate tar cross platform features
-    gompress shall be able to archive files at /some/path/*
+    shall utilize shared cross platform features only
+    shall enable gompress to archive files with input as /some/path/*
         on unix, this will be a list via globbing at shell level
         on windows, the wildcard shall have the list reinterpreted to glob (applied to each)
     the inputs shall be sorted before tar'ed
-    the list is placed in a set to remove duplicate relative paths
-    a common root is identified and the list is reinterpreted as relative to that root
-    the list is passed to tar
-
+    the list passed to tar shall contain unique elements only
+    for multiple files, a common root is identified and the list is reinterpreted as relative to that root
 """
 
 from pathlib import Path
@@ -21,102 +18,206 @@ from debug.mylogging import g_logger
 
 
 def _find_common_root(paths):
-    """looks at each level of the parts and returns the highest common shared level"""
+    """identifies and returns the highest shared root path and its zero-based depth
+
+    starts with depth 0 (root) and examines next depth if all paths share root path
+    in common with the next root at the next depth.
+
+    Args:
+        paths:  a sequence of Path castable objects (e.g. path strings or Paths)
+
+    Process:
+        [ >1 path ]
+        loop:
+            list at depth
+            [ list uniform ]
+            increment depth
+        concatenate parents
+
+        [ 1 path ]
+        concatenate parents
+
+    Returns:
+        Path object of common root
+        depth of common root
+
+    Raises: None
+    """
     from functools import reduce
 
     paths = [Path(path) for path in paths]  # vestigial to list+rewrap into Path
     pathToSharedRoot = None
-    level = 0
+    depth = 0
 
-    #    if len(paths) == 1:
-    #        pathToSharedRoot = paths[0].
     if len(paths) > 1:
         whetherLevelIsShared = True
         while whetherLevelIsShared:
-            partsListAtLevel = [path.parts[level] for path in paths]
+            partsListAtLevel = [path.parts[depth] for path in paths]
             reduced1 = reduce(lambda a, b: a if a == b else None, partsListAtLevel)
             if reduced1 is None:
                 whetherLevelIsShared = False
             else:
                 whetherLevelIsShared = True
-                level += 1
-        shared_level = level - 1
+                depth += 1
+        shared_depth = depth - 1
         root = Path(paths[0].parts[0])
-        pathToSharedRoot = root.joinpath(*paths[0].parts[1 : shared_level + 1])
+        pathToSharedRoot = root.joinpath(*paths[0].parts[1 : shared_depth + 1])
     else:
         pathToSharedRoot = paths[0].parents[0]
-        shared_level = len(paths[0].parts)
-    return pathToSharedRoot, shared_level
+        shared_depth = len(paths[0].parts)
+    return pathToSharedRoot, shared_depth
 
 
 def _strip_root_from_paths(paths, pathToCommonRoots):
+    """maps list unto itself sans paths.
+
+    Args:
+        paths:  stringable sequence of paths (e.g. Path objects)
+        pathToCommonRoots:  stringable path of the parents common to all of \paths\
+
+    Process:
+        map relative common
+        normalize from Path #review
+
+    Returns:
+        the paths input with the common root remapped to "."
+
+    Raises:
+        None
+    """
+
     stripped_paths_str = []
     pathToCommonRootsStr = str(pathToCommonRoots)
     for path in paths:
         path_stripped = str(path).replace(str(pathToCommonRoots), ".")
         stripped_paths_str.append(path_stripped)
+
     return [str(Path(stripped_path_str)) for stripped_path_str in stripped_paths_str]
 
 
-class SelfDestructPath:
-    """Creates temporary directory and constructs&wraps a Path object which is deleted upon garbage collection."""
+class UserTempPath:
+    """creates a temporary directory and prepares a pure Path to a child.
+
+    allows for a temporary directory to persist along with its associated child
+    """
 
     def __init__(self, file):
         self.tempDir = TemporaryDirectory(prefix="gompress_")
         self.data = Path(self.tempDir.name) / file
 
 
-#    def __del__(self):
-#        try:
-#            self.data.unlink()
-#            # self.tempDir.cleanup()
-#        except:
-#            pass
-
-
 def _establish_temporary_tar(files: list, target_basename):
+    """formats a name for the target tar file and returns a temporary directory for it.
+
+    affixes if not present the .tar extension to a basename, which if not provided
+    uses the stem of the first file in the list + '_gompressed'.
+
+    Args:
+        files: Path-castable list of files
+        target_basename: optional name of tar file with or without tar extension
+
+    Process:
+        [ no preference ]
+        pick first stem
+        affix '_gompressed.tar'
+
+        [ preference ]
+        ensure '.tar' ending
+
+    Returns:
+        A UserTempPath object holding an abstract path to the target tar
+
+    Raises:
+        None
+    """
     # pick a basename for the tar file
     if target_basename is None:
-        target_path_str = Path(files[0]).stem
-        if not target_path_str.endswith(".tar"):
-            target_path_str += ".tar"
+        target_path_str = Path(files[0]).stem + "_gompressed.tar"
+        # TODO, check for duplicate name
+
+        # if not target_path_str.endswith(".tar"):
+        #     target_path_str += ".tar"
         target_path = Path(target_path_str)
     else:
         if not target_basename.endswith(".tar"):
             target_basename += ".tar"
         target_path = Path(target_basename)
-    tarFile = SelfDestructPath(target_path)
+    tarFile = UserTempPath(target_path)
     return tarFile
 
 
 def _normalize_input_files(files: list):
-    # on windows, python will not glob when invoked from powershell, manually done here
-    if len(files) == 1 and "*" in files[0]:
-        globexp = Path(files[0]).name
-        baseDirPath = Path(files[0]).parents[0]
-        files = list(baseDirPath.glob(globexp))
-    return files
+    """remap files list input to glob raw * characters
+
+
+    on windows, python will not glob when invoked from powershell, manually done here
+
+    Args:
+        files: Path castable list of files
+
+    Process:
+        on each file
+            [ '*' in name ]
+            expand
+            append
+
+            [ '*' in name ]
+            append
+
+    Returns:
+        a list in which all starred expression have been expanded to their globbed
+        directory listings
+
+    Raises:
+        none
+    """
+
+    remapped_list = []
+    for file in files:
+        if "*" in file:
+            globexp = Path(file).name
+            baseDirPath = Path(file).parents[0]
+            globbed = list(baseDirPath.glob(globexp))
+            remapped_list.extend(globbed)
+        else:
+            remapped_list.append(file)
+
+    # if len(files) == 1 and "*" in files[0]:
+    #     globexp = Path(files[0]).name
+    #     baseDirPath = Path(files[0]).parents[0]
+    #     files = list(baseDirPath.glob(globexp))
+    return remapped_list
 
 
 def archive(files, target_basename=None):
-    """archives files into a temporary tar file and returns as a self deleting user Path object.
+    """archives files or single directory into a temporary tar file and returns
 
+    given a directory or list of files or glob expression (windows), create a tar file of
+    the (expanded) list referenced in a self deleting user Path object UserTempPath.
+    the tar file is named based on the input stem or derived from the first file in the list.
 
-    if target_basename is not provided, the archive is named after the name of the first file
-
+    if //target_basename// is not provided, the archive is named after the name of the first file
     e.g.
     files = ['/tmp/totar/17635.txt', '/tmp/totar/17677.txt', '/tmp/totar/17667.txt']
-    will write a temporary file of the name 17635.tar to a temporary directory and return
+    will write a temporary file of the name 17635_gompressed.tar to a temporary directory and return
     the path to the temporary file
 
+    Args:
+        files: a Path castable sequence of file(s) or on windows glob expressions
 
-    process:
+    Process:
         normalize input
-        make paths
-        find root
-        strip root
+        make unique
+        sort
+        find common root
+        strip common root
         establish temporary tar
 
+    Returns:
+        a UserTempPath object that references the created tar file in a temporary directory
+
+    Raises:
+        None
     """
 
     files = _normalize_input_files(files)
@@ -143,7 +244,6 @@ def archive(files, target_basename=None):
         ]
     )
     g_logger.debug(f"target archive file: {tarFileTarget.data}")
-    # return SelfDestructPath(str(pathToTargetArchive))
     return tarFileTarget
 
 
